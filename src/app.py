41a651f499f53dab1e0cd05d97dfbde71407e24a
Ronan <ronan.mcclorey1@gmail.com>
2020-06-28 10:20:01 -0700

    added basic GUI with tkinter

--- src/app.py
+++ src/app.py
@@ -1,17 +1,37 @@
 import pyautogui
 import pyperclip as clip
 import time
+import tkinter as tk
+from tkinter import ttk
 
+def copyPaste():
+	for i in range(5, 0, -1):
+		otptTimer.set(i)
+		root.update_idletasks()
+		time.sleep(1)
+	otptTimer.set('Pasting!')
+	root.update_idletasks()
+	for word in inptField.get("1.0", "end-1c").split(' '):
+		clip.copy(word)
+		pyautogui.hotkey('ctrl','v')
+		pyautogui.press('enter')
+	
 def main():
-	time.sleep(5)
-	with open('input.txt', 'r') as f:
-		for line in f:
-			for word in line.split(' '):
-				clip.copy(word)
-				pyautogui.hotkey('ctrl','v')
-				pyautogui.press('enter')
+	inptField.pack()
+	button.pack()
+	otpt.pack()
+	root.mainloop()
 	
 
+
+root = tk.Tk()
+root.title("Copy/Paste automation")
+inpt = tk.StringVar()
+inptField = tk.Text(root)
+otptTimer = tk.StringVar()
+otptTimer.set(5)
+button = ttk.Button(root, text="Paste!", command=copyPaste)
+otpt = ttk.Label(root, textvariable=otptTimer)
 
 if __name__ == '__main__':
 	main()
